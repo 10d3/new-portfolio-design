7f875fd976b32f32fbc17e37636a8dfcd888ae80
Herley Antoine <ffast2000@gmail.com>
2025-01-07 09:18:07 -0500

    change base_url

--- app/blog/sitemap.ts
+++ app/blog/sitemap.ts
@@ -5,8 +5,14 @@ import { BASE_URL, formatDate } from "@/lib/utils";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const posts = await getBlogPosts();
-  return posts.map((post) => ({
-    url: `${BASE_URL}/blog/${post.slug}`,
-    lastModified: formatDate(post.metadata.publishedAt),
-  }));
+  return posts.map((post) => {
+    const publishedDate = new Date(post.metadata.publishedAt);
+
+    // Format the date to ISO 8601 string
+    const lastModified = publishedDate.toISOString();
+    return {
+      url: `${BASE_URL}/blog/${post.slug}`,
+      lastModified,
+    };
+  });
 }
